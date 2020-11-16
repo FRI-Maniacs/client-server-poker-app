d4fe484106918e1fc2503d0f777115533fa6eaaf
Andrej Novotny <andrej.novotny97@gmail.com>
2020-11-16 14:52:32 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/poker_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poker_app.dir/main.cpp.o"
+  "poker_app.pdb"
+  "poker_app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poker_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
