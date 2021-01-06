c51c16398fb62be6354ee1cbed6a97a1a33fa739
Andrej Novotny <andrej.novotny97@gmail.com>
2021-01-06 17:54:05 +0100

    Configure CMake

--- cmake-build-debug/CMakeFiles/poker_app.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/poker_app.dir/cmake_clean.cmake
@@ -1,12 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/poker_app.dir/src/main.cpp.o"
-  "CMakeFiles/poker_app.dir/src/server.cpp.o"
-  "CMakeFiles/poker_app.dir/src/client.cpp.o"
-  "poker_app.pdb"
-  "poker_app"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/poker_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
